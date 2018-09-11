869dd15ac6680ac1def9bb2a26a70701642f016c
Marc Mills <mdmills1990@gmail.com>
2018-09-11 23:24:17 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,17 @@
 class Patient
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+  
+
+
 end
